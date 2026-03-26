99212be3993c91392373e5ddfd939d218bd19496
unknown <rwl54@scarletmail.rutgers.edu>
2026-03-25 20:51:19 -0400

    Added read json line to create a json object.

--- C++/C++_Projects/WeatherApp/Weather.h
+++ C++/C++_Projects/WeatherApp/Weather.h
@@ -4,15 +4,21 @@
 #include <cstdlib>
 #include <nlohmann/json.hpp>
 #include <curl/curl.h>
-
+using json = nlohmann::json;
 
 class WeatherData {
     public: 
         std::string data;
         WeatherData(std::string result) : data(result) {}
 
-
         inline void print() {
             std::cout << data << "\n";
         }
+
+        inline void readAsJSON() {
+            json Doc{json::parse(data)};
+            std::cout << Doc;
+        }
+
+        
 };
