ae7e75d7d56bde72835e51fc740784f2dd6389b2
Ryan Lamb <ryanwlamb0109@gmail.com>
2026-04-17 14:51:12 -0400

    Gave an example use case of thread guard.

--- C++/Concurrency/ThreadGuard.cpp
+++ C++/Concurrency/ThreadGuard.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+
 class thread_guard {
 
     std::thread& t;
@@ -17,3 +19,32 @@ class thread_guard {
 
 
 };
+
+
+void task() {
+    std::cout << "This task is running." << "\n";
+
+    const char frames[] = {'\\','|','/' , '-'};
+
+    for (int i = 0; i < 20; i++) {
+        std::cout << frames [i%4] << "\b" << std::flush;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+
+    std::cout << "task complete!" << "\n";
+}
+
+
+int main() {
+    std::thread t1(task);
+    thread_guard guard(t1);
+
+
+    if (t1.joinable()) {
+        t1.join();
+    }
+
+
+}
