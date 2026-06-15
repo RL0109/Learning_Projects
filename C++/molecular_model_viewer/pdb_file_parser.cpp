1e2d1cb9be9425d0b81c61774ae05052ec5d3bb2
unknown <rwl54@scarletmail.rutgers.edu>
2026-06-14 22:33:38 -0400

    Implemented a vector of vector of strings that stores the files input.

--- C++/molecular_model_viewer/pdb_file_parser.cpp
+++ C++/molecular_model_viewer/pdb_file_parser.cpp
@@ -2,7 +2,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
-using std::ifstream, std::cout, std::cerr, std::cin, std::string;
+#include <sstream>
+#include "raylib.h"
+#include "atom.h"
+
+using std::ifstream, std::cout, std::cerr, std::cin, std::string, std::vector;
 
 class PDBFileParser {
     
@@ -11,21 +15,78 @@ class PDBFileParser {
 
 };
 
+vector<string> lineParser(const string line) {
+
+    vector<string> row;
+
+    string str;
+
+    std::stringstream ss(line);
+
+    while (ss >> str) {
+        cout << "string steam: " << str << "\n";
+        row.push_back(str);
+    }
+
+    return row;
+} 
+
+void readData (const vector<vector<string>> dataVector) {
+
+    for (auto dv : dataVector) {
+        for (auto d : dv) {
+            std::cout << d << " ";
+        }
+        cout << "\n";
+    }
+
+}
+
+
 int main() {
     //Open text file
-    ifstream f("LEU.cif");
+    ifstream moleculeFile("LEU.cif");
+    string line; 
 
-    if (!f.is_open()) {
-        cerr << "Error opening the file!";
-        return 1;
+    bool insideTargetLoop = false;
+    int columnCount = 0;
+    bool finishedHeader = false;
 
+    vector<vector<string>> moleculedata;
+    vector<string> columnHeader;
+    vector<Vector3> coordinates;
+
+
+    while (std::getline(moleculeFile , line)) {
+        if (line.rfind("_chem_comp_atom.comp_id", 0) == 0) {
+            insideTargetLoop = true;
+        }
+        
+        if (insideTargetLoop && !line.empty()) {
+            if (line[0] == '_') {
+                columnHeader.push_back(line);
+            }
+            if (line[0] != '_') {
+                if (!finishedHeader) {
+                    moleculedata.push_back(columnHeader);
+                    finishedHeader = true;
+                }
+                if (line[0] == '#') {
+                    break;
+                }
+                moleculedata.push_back(lineParser(line));
+                cout << line << "\n";
+            }
+        }
     }
-    string s;   
+      
+    cout << "Reading data" << "\n";
+    readData(moleculedata);
+    cout << moleculedata[0][12];
+    cout << moleculedata[0][13];
+    cout << moleculedata[0][14];
 
-    while (getline(f,s))
-        cout << s << "\n";
-
-    f.close();
+    moleculeFile.close();
     return 0;
 
 }
